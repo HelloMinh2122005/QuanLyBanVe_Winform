a8dee07b526796c4d2430db671e6e9fa49a09ff4
helloMinh <phandinhminh48@gmail.com>
2024-10-28 23:18:25 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,408 @@
+﻿namespace QuanLyBanVe
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            Ghe1 = new Button();
+            Ghe2 = new Button();
+            Ghe3 = new Button();
+            Ghe4 = new Button();
+            Ghe5 = new Button();
+            Ghe6 = new Button();
+            Ghe7 = new Button();
+            Ghe8 = new Button();
+            Ghe9 = new Button();
+            Ghe10 = new Button();
+            Ghe11 = new Button();
+            Ghe12 = new Button();
+            Ghe14 = new Button();
+            Ghe15 = new Button();
+            Ghe13 = new Button();
+            ThanhTien_TEXT = new TextBox();
+            ThanhTien_RES = new TextBox();
+            Chon_BUT = new Button();
+            HuyBo_BUT = new Button();
+            KetThuc_BUT = new Button();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label1.BackColor = Color.FromArgb(192, 255, 255);
+            label1.BorderStyle = BorderStyle.Fixed3D;
+            label1.Font = new Font("Arial", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Navy;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(1020, 40);
+            label1.TabIndex = 1;
+            label1.Text = "MÀN ẢNH";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            tableLayoutPanel1.CellBorderStyle = TableLayoutPanelCellBorderStyle.OutsetPartial;
+            tableLayoutPanel1.ColumnCount = 5;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.Controls.Add(Ghe1, 0, 0);
+            tableLayoutPanel1.Controls.Add(Ghe2, 1, 0);
+            tableLayoutPanel1.Controls.Add(Ghe3, 2, 0);
+            tableLayoutPanel1.Controls.Add(Ghe4, 3, 0);
+            tableLayoutPanel1.Controls.Add(Ghe5, 4, 0);
+            tableLayoutPanel1.Controls.Add(Ghe6, 0, 1);
+            tableLayoutPanel1.Controls.Add(Ghe7, 1, 1);
+            tableLayoutPanel1.Controls.Add(Ghe8, 2, 1);
+            tableLayoutPanel1.Controls.Add(Ghe9, 3, 1);
+            tableLayoutPanel1.Controls.Add(Ghe10, 4, 1);
+            tableLayoutPanel1.Controls.Add(Ghe11, 0, 2);
+            tableLayoutPanel1.Controls.Add(Ghe12, 1, 2);
+            tableLayoutPanel1.Controls.Add(Ghe14, 3, 2);
+            tableLayoutPanel1.Controls.Add(Ghe15, 4, 2);
+            tableLayoutPanel1.Controls.Add(Ghe13, 2, 2);
+            tableLayoutPanel1.Location = new Point(66, 105);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 33.3333321F));
+            tableLayoutPanel1.Size = new Size(931, 274);
+            tableLayoutPanel1.TabIndex = 1;
+            tableLayoutPanel1.Paint += tableLayoutPanel1_Paint;
+            // 
+            // Ghe1
+            // 
+            Ghe1.BackColor = SystemColors.ButtonFace;
+            Ghe1.Font = new Font("Arial Narrow", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Ghe1.ForeColor = Color.Black;
+            Ghe1.Location = new Point(6, 6);
+            Ghe1.Name = "Ghe1";
+            Ghe1.Size = new Size(176, 81);
+            Ghe1.TabIndex = 10;
+            Ghe1.Text = "1";
+            Ghe1.UseVisualStyleBackColor = false;
+            Ghe1.Click += Ghe_Click;
+            // 
+            // Ghe2
+            // 
+            Ghe2.BackColor = SystemColors.ButtonFace;
+            Ghe2.Font = new Font("Arial Narrow", 19.8000011F, FontStyle.Bold);
+            Ghe2.ForeColor = Color.Black;
+            Ghe2.Location = new Point(191, 6);
+            Ghe2.Name = "Ghe2";
+            Ghe2.Size = new Size(176, 81);
+            Ghe2.TabIndex = 10;
+            Ghe2.Text = "2";
+            Ghe2.UseVisualStyleBackColor = false;
+            Ghe2.Click += Ghe_Click;
+            // 
+            // Ghe3
+            // 
+            Ghe3.BackColor = SystemColors.ButtonFace;
+            Ghe3.Font = new Font("Arial Narrow", 19.8000011F, FontStyle.Bold);
+            Ghe3.ForeColor = Color.Black;
+            Ghe3.Location = new Point(376, 6);
+            Ghe3.Name = "Ghe3";
+            Ghe3.Size = new Size(176, 81);
+            Ghe3.TabIndex = 10;
+            Ghe3.Text = "3";
+            Ghe3.UseVisualStyleBackColor = false;
+            Ghe3.Click += Ghe_Click;
+            // 
+            // Ghe4
+            // 
+            Ghe4.BackColor = SystemColors.ButtonFace;
+            Ghe4.Font = new Font("Arial Narrow", 19.8000011F, FontStyle.Bold);
+            Ghe4.ForeColor = Color.Black;
+            Ghe4.Location = new Point(561, 6);
+            Ghe4.Name = "Ghe4";
+            Ghe4.Size = new Size(176, 81);
+            Ghe4.TabIndex = 10;
+            Ghe4.Text = "4";
+            Ghe4.UseVisualStyleBackColor = false;
+            Ghe4.Click += Ghe_Click;
+            // 
+            // Ghe5
+            // 
+            Ghe5.BackColor = SystemColors.ButtonFace;
+            Ghe5.Font = new Font("Arial Narrow", 19.8000011F, FontStyle.Bold);
+            Ghe5.ForeColor = Color.Black;
+            Ghe5.Location = new Point(746, 6);
+            Ghe5.Name = "Ghe5";
+            Ghe5.Size = new Size(179, 81);
+            Ghe5.TabIndex = 10;
+            Ghe5.Text = "5";
+            Ghe5.UseVisualStyleBackColor = false;
+            Ghe5.Click += Ghe_Click;
+            // 
+            // Ghe6
+            // 
+            Ghe6.BackColor = SystemColors.ButtonFace;
+            Ghe6.Font = new Font("Arial Narrow", 19.8000011F, FontStyle.Bold);
+            Ghe6.ForeColor = Color.Black;
+            Ghe6.Location = new Point(6, 96);
+            Ghe6.Name = "Ghe6";
+            Ghe6.Size = new Size(176, 81);
+            Ghe6.TabIndex = 10;
+            Ghe6.Text = "6";
+            Ghe6.UseVisualStyleBackColor = false;
+            Ghe6.Click += Ghe_Click;
+            // 
+            // Ghe7
+            // 
+            Ghe7.BackColor = SystemColors.ButtonFace;
+            Ghe7.Font = new Font("Arial Narrow", 19.8000011F, FontStyle.Bold);
+            Ghe7.ForeColor = Color.Black;
+            Ghe7.Location = new Point(191, 96);
+            Ghe7.Name = "Ghe7";
+            Ghe7.Size = new Size(176, 81);
+            Ghe7.TabIndex = 10;
+            Ghe7.Text = "7";
+            Ghe7.UseVisualStyleBackColor = false;
+            Ghe7.Click += Ghe_Click;
+            // 
+            // Ghe8
+            // 
+            Ghe8.BackColor = SystemColors.ButtonFace;
+            Ghe8.Font = new Font("Arial Narrow", 19.8000011F, FontStyle.Bold);
+            Ghe8.ForeColor = Color.Black;
+            Ghe8.Location = new Point(376, 96);
+            Ghe8.Name = "Ghe8";
+            Ghe8.Size = new Size(176, 81);
+            Ghe8.TabIndex = 10;
+            Ghe8.Text = "8";
+            Ghe8.UseVisualStyleBackColor = false;
+            Ghe8.Click += Ghe_Click;
+            // 
+            // Ghe9
+            // 
+            Ghe9.BackColor = SystemColors.ButtonFace;
+            Ghe9.Font = new Font("Arial Narrow", 19.8000011F, FontStyle.Bold);
+            Ghe9.ForeColor = Color.Black;
+            Ghe9.Location = new Point(561, 96);
+            Ghe9.Name = "Ghe9";
+            Ghe9.Size = new Size(176, 81);
+            Ghe9.TabIndex = 10;
+            Ghe9.Text = "9";
+            Ghe9.UseVisualStyleBackColor = false;
+            Ghe9.Click += Ghe_Click;
+            // 
+            // Ghe10
+            // 
+            Ghe10.BackColor = SystemColors.ButtonFace;
+            Ghe10.Font = new Font("Arial Narrow", 19.8000011F, FontStyle.Bold);
+            Ghe10.ForeColor = Color.Black;
+            Ghe10.Location = new Point(746, 96);
+            Ghe10.Name = "Ghe10";
+            Ghe10.Size = new Size(179, 81);
+            Ghe10.TabIndex = 10;
+            Ghe10.Text = "10";
+            Ghe10.UseVisualStyleBackColor = false;
+            Ghe10.Click += Ghe_Click;
+            // 
+            // Ghe11
+            // 
+            Ghe11.BackColor = SystemColors.ButtonFace;
+            Ghe11.Font = new Font("Arial Narrow", 19.8000011F, FontStyle.Bold);
+            Ghe11.ForeColor = Color.Black;
+            Ghe11.Location = new Point(6, 186);
+            Ghe11.Name = "Ghe11";
+            Ghe11.Size = new Size(176, 82);
+            Ghe11.TabIndex = 10;
+            Ghe11.Text = "11";
+            Ghe11.UseVisualStyleBackColor = false;
+            Ghe11.Click += Ghe_Click;
+            // 
+            // Ghe12
+            // 
+            Ghe12.BackColor = SystemColors.ButtonFace;
+            Ghe12.Font = new Font("Arial Narrow", 19.8000011F, FontStyle.Bold);
+            Ghe12.ForeColor = Color.Black;
+            Ghe12.Location = new Point(191, 186);
+            Ghe12.Name = "Ghe12";
+            Ghe12.Size = new Size(176, 82);
+            Ghe12.TabIndex = 10;
+            Ghe12.Text = "12";
+            Ghe12.UseVisualStyleBackColor = false;
+            Ghe12.Click += Ghe_Click;
+            // 
+            // Ghe14
+            // 
+            Ghe14.BackColor = SystemColors.ButtonFace;
+            Ghe14.Font = new Font("Arial Narrow", 19.8000011F, FontStyle.Bold);
+            Ghe14.ForeColor = Color.Black;
+            Ghe14.Location = new Point(561, 186);
+            Ghe14.Name = "Ghe14";
+            Ghe14.Size = new Size(176, 82);
+            Ghe14.TabIndex = 10;
+            Ghe14.Text = "14";
+            Ghe14.UseVisualStyleBackColor = false;
+            Ghe14.Click += Ghe_Click;
+            // 
+            // Ghe15
+            // 
+            Ghe15.BackColor = SystemColors.ButtonFace;
+            Ghe15.Font = new Font("Arial Narrow", 19.8000011F, FontStyle.Bold);
+            Ghe15.ForeColor = Color.Black;
+            Ghe15.Location = new Point(746, 186);
+            Ghe15.Name = "Ghe15";
+            Ghe15.Size = new Size(179, 82);
+            Ghe15.TabIndex = 10;
+            Ghe15.Text = "15";
+            Ghe15.UseVisualStyleBackColor = false;
+            Ghe15.Click += Ghe_Click;
+            // 
+            // Ghe13
+            // 
+            Ghe13.BackColor = SystemColors.ButtonFace;
+            Ghe13.Font = new Font("Arial Narrow", 19.8000011F, FontStyle.Bold);
+            Ghe13.ForeColor = Color.Black;
+            Ghe13.Location = new Point(376, 186);
+            Ghe13.Name = "Ghe13";
+            Ghe13.Size = new Size(176, 82);
+            Ghe13.TabIndex = 10;
+            Ghe13.Text = "13";
+            Ghe13.UseVisualStyleBackColor = false;
+            Ghe13.Click += Ghe_Click;
+            // 
+            // ThanhTien_TEXT
+            // 
+            ThanhTien_TEXT.BackColor = SystemColors.Control;
+            ThanhTien_TEXT.Enabled = false;
+            ThanhTien_TEXT.Font = new Font("Arial Narrow", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            ThanhTien_TEXT.Location = new Point(257, 403);
+            ThanhTien_TEXT.Name = "ThanhTien_TEXT";
+            ThanhTien_TEXT.Size = new Size(160, 45);
+            ThanhTien_TEXT.TabIndex = 2;
+            ThanhTien_TEXT.Text = "Thành tiền:";
+            // 
+            // ThanhTien_RES
+            // 
+            ThanhTien_RES.Enabled = false;
+            ThanhTien_RES.Font = new Font("Arial Narrow", 19.8000011F, FontStyle.Bold);
+            ThanhTien_RES.Location = new Point(449, 403);
+            ThanhTien_RES.Name = "ThanhTien_RES";
+            ThanhTien_RES.ReadOnly = true;
+            ThanhTien_RES.Size = new Size(354, 45);
+            ThanhTien_RES.TabIndex = 3;
+            ThanhTien_RES.Text = "0";
+            // 
+            // Chon_BUT
+            // 
+            Chon_BUT.BackColor = SystemColors.ButtonFace;
+            Chon_BUT.Font = new Font("Arial Narrow", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Chon_BUT.ForeColor = Color.Black;
+            Chon_BUT.Location = new Point(257, 472);
+            Chon_BUT.Name = "Chon_BUT";
+            Chon_BUT.Size = new Size(159, 44);
+            Chon_BUT.TabIndex = 4;
+            Chon_BUT.Text = "Chọn";
+            Chon_BUT.UseVisualStyleBackColor = false;
+            Chon_BUT.Click += Chon_BUT_Click;
+            // 
+            // HuyBo_BUT
+            // 
+            HuyBo_BUT.BackColor = SystemColors.ButtonFace;
+            HuyBo_BUT.Font = new Font("Arial Narrow", 16.2F, FontStyle.Bold);
+            HuyBo_BUT.ForeColor = Color.Black;
+            HuyBo_BUT.Location = new Point(459, 472);
+            HuyBo_BUT.Name = "HuyBo_BUT";
+            HuyBo_BUT.Size = new Size(159, 44);
+            HuyBo_BUT.TabIndex = 5;
+            HuyBo_BUT.Text = "Hủy bỏ";
+            HuyBo_BUT.UseVisualStyleBackColor = false;
+            HuyBo_BUT.Click += HuyBo_BUT_Click;
+            // 
+            // KetThuc_BUT
+            // 
+            KetThuc_BUT.BackColor = SystemColors.ButtonFace;
+            KetThuc_BUT.Font = new Font("Arial Narrow", 16.2F, FontStyle.Bold);
+            KetThuc_BUT.ForeColor = Color.Black;
+            KetThuc_BUT.Location = new Point(644, 472);
+            KetThuc_BUT.Name = "KetThuc_BUT";
+            KetThuc_BUT.Size = new Size(159, 44);
+            KetThuc_BUT.TabIndex = 6;
+            KetThuc_BUT.Text = "Kết thúc";
+            KetThuc_BUT.UseVisualStyleBackColor = false;
+            KetThuc_BUT.Click += KetThuc_BUT_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.Window;
+            ClientSize = new Size(1044, 548);
+            Controls.Add(KetThuc_BUT);
+            Controls.Add(HuyBo_BUT);
+            Controls.Add(Chon_BUT);
+            Controls.Add(ThanhTien_RES);
+            Controls.Add(ThanhTien_TEXT);
+            Controls.Add(tableLayoutPanel1);
+            Controls.Add(label1);
+            ForeColor = Color.SandyBrown;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            tableLayoutPanel1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button Ghe1;
+        private Button Ghe2;
+        private Button Ghe3;
+        private Button Ghe4;
+        private Button Ghe5;
+        private Button Ghe6;
+        private Button Ghe7;
+        private Button Ghe8;
+        private Button Ghe9;
+        private Button Ghe10;
+        private Button Ghe11;
+        private Button Ghe12;
+        private Button Ghe13;
+        private Button Ghe14;
+        private Button Ghe15;
+        private TextBox ThanhTien_TEXT;
+        private TextBox ThanhTien_RES;
+        private Button Chon_BUT;
+        private Button HuyBo_BUT;
+        private Button KetThuc_BUT;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿namespace QuanLyBanVe
+{
+    public partial class Form1 : Form
+    {
+        private List<Button> DS_GheDangChon = new List<Button>();
+        private Dictionary<Button, int> TriGiaGhe = new Dictionary<Button, int>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TriGiaGhe.Add(Ghe1, 8000);
+            TriGiaGhe.Add(Ghe2, 8000);
+            TriGiaGhe.Add(Ghe3, 8000);
+            TriGiaGhe.Add(Ghe4, 8000);
+            TriGiaGhe.Add(Ghe5, 8000);
+            TriGiaGhe.Add(Ghe6, 6500);
+            TriGiaGhe.Add(Ghe7, 6500);
+            TriGiaGhe.Add(Ghe8, 6500);
+            TriGiaGhe.Add(Ghe9, 6500);
+            TriGiaGhe.Add(Ghe10, 6500);
+            TriGiaGhe.Add(Ghe11, 5000);
+            TriGiaGhe.Add(Ghe12, 5000);
+            TriGiaGhe.Add(Ghe13, 5000);
+            TriGiaGhe.Add(Ghe14, 5000);
+            TriGiaGhe.Add(Ghe15, 5000);
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+
+        private void Chon_BUT_Click(object sender, EventArgs e)
+        {
+            if (DS_GheDangChon.Count == 0)
+                MessageBox.Show("Bạn chưa chọn ghế nào để bán");
+            else
+            {
+                int TongTien = 0;
+                foreach (var Ghe in DS_GheDangChon)
+                {
+                    TongTien += TriGiaGhe[Ghe];
+                    Ghe.BackColor = Color.Yellow;
+                }
+                ThanhTien_RES.Text = TongTien.ToString();
+                DS_GheDangChon.Clear();
+            }
+        }
+
+        private void Ghe_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (btn.BackColor == Color.Blue)
+            {
+                btn.BackColor = SystemColors.ButtonFace;
+                DS_GheDangChon.Remove(btn);
+            }
+            else
+            {
+                if (btn.BackColor != Color.Yellow)
+                {
+                    btn.BackColor = Color.Blue;
+                    DS_GheDangChon.Add(btn);
+                }
+                else
+                {
+                    string GheSo = btn.Name.Replace("Ghe", "");
+                    MessageBox.Show($"Vị trí {GheSo} đã được bán");
+                }
+            }
+        }
+        private void HuyBo_BUT_Click(object sender, EventArgs e)
+        {
+            foreach (var Ghe in DS_GheDangChon)
+                if (Ghe.BackColor == Color.Blue)
+                    Ghe.BackColor = SystemColors.ButtonFace;
+            ThanhTien_RES.Text = "0";
+        }
+
+        private void KetThuc_BUT_Click(object sender, EventArgs e)
+        {
+            foreach (var Ghe in TriGiaGhe.Keys)
+                Ghe.BackColor = SystemColors.ButtonFace;
+            DS_GheDangChon.Clear();
+            ThanhTien_RES.Text = "0";
+        }
+    }
+}
